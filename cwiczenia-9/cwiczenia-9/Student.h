08ac70bc8ec5bbec159e39c7787be2097317bc1b
procek69 <llukas.de@gmail.com>
2019-05-17 09:03:53 +0200

    Ćwiczenia 9

---
+++ cwiczenia-9/cwiczenia-9/Student.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <vector>
+
+class StudentGroup;
+
+class Student
+{
+	int s; // np. s00000
+public:
+	std::vector<StudentGroup*> groups;
+	void detach(StudentGroup*);
+	Student();
+	Student(int);
+	~Student();
+};
+
---
+++ cwiczenia-9/cwiczenia-9/StudentGroup.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "StudentGroup.h"
+#include <iostream>
+
+void StudentGroup::add(Student *student)
+{
+	std::cout << this->name << ": Dodawanie studenta" << std::endl;
+	students.push_back(student);
+	student->groups.push_back(this);
+}
+
+void StudentGroup::remove(Student *student)
+{
+	std::vector<Student*>::iterator position = std::find(students.begin(), students.end(), student);
+	if (position != students.end()) {
+		std::cout << this->name << ": Usuwanie dowiazania grupy u studenta" << std::endl;
+		students.erase(position);
+
+		if (!students.size()) {
+			std::cout << this->name << ": Usuwanie grupy" << std::endl;
+			delete this;
+		}
+		return;
+	}
+}
+
+StudentGroup::StudentGroup(std::string name)
+{
+	this->name = name;
+}
+
+StudentGroup::~StudentGroup()
+{
+	std::cout << this->name << ": Grupa studencka zostala usunieta" << std::endl;
+}
