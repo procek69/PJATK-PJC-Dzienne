df8882896ff74982e871e2b3e7b7b65e081741f7
procek69 <llukas.de@gmail.com>
2019-06-03 16:09:55 +0200

    Ćwiczenia 10

---
+++ cwiczenia-10/Node.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "Node.h"
+#include <iostream>
+
+Node::~Node()
+{
+	std::cout << "~Node(" << _value << ")" << std::endl;
+}
+
+void Node::push_back(int value)
+{
+	// TODO: napisac bez rekurencji
+	if (next != nullptr)
+		return next->push_back(value);
+	next = std::make_shared<Node>(value);
+	next->previous = this;
+}
